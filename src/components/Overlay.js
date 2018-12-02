2b8b38a6fa2e4d93861504b55e3adf796a3a3655
littlestone <littlestone@suboat.com>
2018-12-02 21:41:16 +0800

    fix: fix overlay component style and add Animated

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,50 +1,129 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 import {
   View,
   StyleSheet,
   Dimensions,
   Platform,
+  TouchableOpacity,
   TouchableWithoutFeedback,
-} from 'react-native';
-import { ViewPropTypes, withTheme } from '../config';
+  Text,
+  Animated,
+  Easing
+} from "react-native";
+import { ViewPropTypes, withTheme } from "../config";
 
-const dimensions = Dimensions.get('window');
+const dimensions = Dimensions.get("window");
 const windowWidth = dimensions.width;
 const windowHeight = dimensions.height;
 
-const Overlay = props => {
-  const {children, isVisible, containerStyle, overLayStyle, windowBackgroundColor, overlayBackgroundColor,onBackdropPress, borderRadius, width,height,fullScreen,...rest} = props;
-  if (!isVisible) return null;
-  return (
-    <TouchableWithoutFeedback onPress={onBackdropPress}>
-      <View
-      style={StyleSheet.flatten([
-        styles.container,
-        {backgroundColor: windowBackgroundColor},
-        containerStyle
-      ])} {...rest}>
-        <TouchableWithoutFeedback>
-          <View style={StyleSheet.flatten([
-            styles.overlay,
-            {
-              borderRadius,
-              backgroundColor: overlayBackgroundColor,
-              width,
-              height,
-            },
-              fullScreen && {width: windowWidth, height: windowHeight},
-              overLayStyle
-          ])}>
-            {children}
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-    </TouchableWithoutFeedback>
-  )
-};
+export default class Overlay extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spinValue : new Animated.Value(0)
+    };
+  }
 
-export default Overlay;
+  componentDidMount () {
+    this.spin()
+  };
+
+  spin () {
+    const { spinValue } = this.state;
+    spinValue.setValue(0.3);
+    Animated.timing(
+      spinValue,
+      {
+        toValue: 1,
+        duration: 300,
+        easing: Easing.linear
+      }
+    ).start()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isVisible === false) {
+      const { spinValue } = this.state;
+      Animated.timing(
+        spinValue,
+        {
+          toValue: 0,
+          duration: 250,
+          easing: Easing.linear
+        }
+      ).start()
+    }
+    if (nextProps.isVisible === true) {
+      this.spin()
+    }
+  }
+
+  render() {
+    const { spinValue } = this.state;
+    console.log("** spinValue **", spinValue);
+
+    const {
+      children,
+      isVisible,
+      containerStyle,
+      overLayStyle,
+      windowBackgroundColor,
+      overlayBackgroundColor,
+      onBackdropPress,
+      onSubmit,
+      borderRadius,
+      width,
+      height,
+      fullScreen,
+      ...rest
+    } = this.props;
+    if (!isVisible) return null;
+    return (
+      <TouchableWithoutFeedback onPress={onBackdropPress}>
+        <Animated.View
+          style={StyleSheet.flatten([
+            styles.container,
+            {
+              backgroundColor: windowBackgroundColor,
+              opacity: spinValue
+            },
+            containerStyle
+          ])}
+          {...rest}
+        >
+          <TouchableWithoutFeedback>
+            <Animated.View
+              style={StyleSheet.flatten([
+                styles.overlay,
+                {
+                  borderRadius,
+                  backgroundColor: overlayBackgroundColor,
+                  width,
+                  height,
+                  opacity: spinValue,
+                  transform:[{ scale: spinValue }]
+                },
+                fullScreen && { width: windowWidth, height: windowHeight },
+                overLayStyle
+              ])}
+            >
+              {children}
+              <View style={styles.btnView}>
+                <TouchableOpacity style={[styles.btn, styles.cancelBtn]} onPress={() => onBackdropPress()}>
+                  <Text style={styles.btnText}>取消</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.btn, styles.confirmBtn]} onPress={() => onSubmit()}>
+                  <Text style={[styles.btnText, styles.confirmBtnText]}>确定</Text>
+                </TouchableOpacity>
+              </View>
+            </Animated.View>
+          </TouchableWithoutFeedback>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
 
 Overlay.propTypes = {
   children: PropTypes.any,
@@ -57,36 +136,68 @@ Overlay.propTypes = {
   borderRadius: PropTypes.number,
   width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  fullScreen: PropTypes.bool,
+  fullScreen: PropTypes.bool
 };
 
 Overlay.defaultProps = {
-  windowBackgroundColor: 'rgba(0,0,0,.4)',
-  overlayBackgroundColor: '#fff',
-  width: windowWidth - 80,
-  height: windowHeight - 80,
+  windowBackgroundColor: "rgba(0,0,0,.4)",
+  overlayBackgroundColor: "#fff",
+  borderRadius: 10,
+  width: windowWidth*0.8,
+  height: windowHeight*0.4,
   onBackdropPress: () => null,
+  onSubmit: () => null,
 };
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     width: windowWidth,
     height: windowHeight,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   overlay: {
-    borderRadius: 5,
+    borderRadius: 10,
     padding: 10,
     ...Platform.select({
       ios: {
-        shadowColor: 'rgba(0,0,0,.3)',
-        shadowOffset: {width:0, height: 1},
+        shadowColor: "rgba(0,0,0,.3)",
+        shadowOffset: { width: 0, height: 1 },
         shadowRadius: 4
       }
     })
+  },
+  btnView: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flexDirection: 'row',
+  },
+  btn: {
+    height: 56,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopColor: '#ddd',
+    borderTopWidth: 1,
+  },
+  cancelBtn: {
+    borderBottomLeftRadius: 10,
+    borderRightColor: '#ddd',
+    borderRightWidth: 1
+  },
+  confirmBtn: {
+    borderBottomRightRadius: 10,
+  },
+  btnText: {
+    fontSize: 16,
+    color: '#999'
+  },
+  confirmBtnText: {
+    color: '#1296db'
   }
 });
